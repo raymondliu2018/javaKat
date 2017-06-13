c4a1560cf76be915edb2f1e8e795d4a7ef83a86e
raymondliu2018 <raymondliu2018@gmail.com>
2017-06-12 20:17:07 -0700

    Create GameBuilder-Windows.bat

--- Download Me/GameBuilder-Windows.bat
+++ Download Me/GameBuilder-Windows.bat
@@ -1,10 +1,10 @@
 @echo off
 title GameBuilder
 md GAME_TITLE
-javac -cp thePackage.jar workspace/*.java
-jar cfvm GAME_TITLE.jar Manifest.txt thePackage.jar workspace/*.class images/* sounds/*
+javac -cp javaKat.jar workspace/*.java
+jar cfvm GAME_TITLE.jar Manifest.txt javaKat.jar workspace/*.class images/* sounds/*
 move GAME_TITLE.jar GAME_TITLE
-copy thePackage.jar GAME_TITLE
+copy javaKat.jar GAME_TITLE
 cd GAME_TITLE
-attrib +h thePackage.jar
+attrib +h javaKat.jar
 pause
